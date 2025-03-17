d6886f42c4af73dcd34f9931f318ea7f9ab71df2
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2025-03-17 11:24:21 -0300

    console .log .warm .error

---
+++ app.js
@@ -0,0 +1,3 @@
+console.log("Información: Todo funciona correctamente.");
+console.warn("Advertencia: Este valor puede causar problemas.");
+console.error("Error: No se pudo completar la operación.");
