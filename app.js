cf1920fdd41e2b4b207dde2fecff57acf0727802
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2025-03-27 16:59:19 -0300

    arreglos

--- app.js
+++
@@ -1,6 +0,0 @@
-let a = 10;
-let b = '5';
-let c = 5;
-console.table({a,b,c})
-
-
---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+const colores = new Array();
+colores[0] = "rojo";
+colores[1] = "verde";
+colores[2] = "azul";
+colores[3] = "cafe";
+console.log(colores);
+colores.forEach(color => {
+    console.log(color);
+});
+
+
+
+
